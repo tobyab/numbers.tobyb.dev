4a823379bbfc4d59235b62385ee7c57b6fdb377f
Toby Brown <hi@tobyb.dev>
2023-08-16 14:31:20 +0100

    didn't work, trying a new approach!

---
+++ pages/500.tsx
@@ -0,0 +1,7 @@
+export default function FiveHunderd() {
+    return (
+        <div className="place-items-center justify-center grid h-screen">
+            <h1 className="font-semibold text-4xl max-w-2xl text-center">something&apos;s gone wrong. try reloading the page.</h1>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,50 @@ import Link from "next/link";
 import Head from "next/head";
 
 export default function Home({ dispatch, ambulance, fire, police, country }) {
+  if (!dispatch || !ambulance || !fire || !police || !country) {
+    return (
+      <div>
+        <Head>
+          <title>Emergency Numbers</title>
+          <meta property="og:title" content="Emergency Numbers" />
+          <meta
+            name="description"
+            content="What's the emergency number for the country you're in?"
+          />
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+          <meta property="og:url" content="https://numbers.tobyb.dev" />
+          <meta property="og:type" content="website" />
+        </Head>
+        <div className="grid place-items-center justify-center h-screen text-center mx-8">
+          <div>
+            <h1 className="text-4xl max-w-2xl lowercase">
+              something&apos;s gone wrong. try reloading the page.
+            </h1>
+            <p className="absolute inset-x-0 bottom-8">
+              a tiny utility built by{" "}
+              <Link href="https://tobyb.dev" className="underline">
+                toby
+              </Link>
+              ! ✨
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Head>
         <title>Emergency Numbers</title>
-          <meta property="og:title" content="Emergency Numbers" />
-          <meta name="description" content="What's the emergency number for the country you're in?" />
-          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-          <meta property="og:url" content="https://numbers.tobyb.dev" />
-          <meta property="og:type" content="website" />
+        <meta property="og:title" content="Emergency Numbers" />
+        <meta
+          name="description"
+          content="What's the emergency number for the country you're in?"
+        />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <meta property="og:url" content="https://numbers.tobyb.dev" />
+        <meta property="og:type" content="website" />
       </Head>
       <div className="grid place-items-center justify-center h-screen text-center mx-8">
         <div>
@@ -21,32 +56,16 @@ export default function Home({ dispatch, ambulance, fire, police, country }) {
           </h1>
           <div className="my-4 sm:flex text-center justify-center place-items-center sm:space-x-16 space-y-4 sm:space-y-0">
             {dispatch.all[0] === "" ? null : (
-                <Data
-                    data={dispatch}
-                    emoji="📞"
-                    name="dispatch"
-                />
+              <Data data={dispatch} emoji="📞" name="dispatch" />
             )}
             {ambulance.all[0] === "" ? null : (
-                <Data
-                    data={police}
-                    emoji="🚑"
-                    name="ambulance"
-                />
+              <Data data={police} emoji="🚑" name="ambulance" />
             )}
             {fire.all[0] === "" ? null : (
-                <Data
-                    data={fire}
-                    emoji="🚒"
-                    name="fire"
-                />
+              <Data data={fire} emoji="🚒" name="fire" />
             )}
             {police.all[0] === "" ? null : (
-               <Data
-                  data={police}
-                  emoji="🚓"
-                  name="police"
-               />
+              <Data data={police} emoji="🚓" name="police" />
             )}
           </div>
           <p className="absolute inset-x-0 bottom-8">
@@ -63,11 +82,13 @@ export default function Home({ dispatch, ambulance, fire, police, country }) {
 }
 
 function Data({ data, emoji, name }) {
-  return(
-      <div className="grid space-y-2">
-        <h1 className="font-bold text-6xl">{data.all}</h1>
-        <p>{emoji} {name}</p>
-      </div>
+  return (
+    <div className="grid space-y-2">
+      <h1 className="font-bold text-6xl">{data.all}</h1>
+      <p>
+        {emoji} {name}
+      </p>
+    </div>
   );
 }
 
@@ -96,14 +117,6 @@ export async function getServerSideProps({ req, res }) {
       },
     };
   } else {
-    return {
-        props: {
-            dispatch: "Loading...",
-            ambulance: "Loading...",
-            fire: "Loading...",
-            police: "Loading...",
-            country: "Loading...",
-        }
-    }
+    return {};
   }
 }
